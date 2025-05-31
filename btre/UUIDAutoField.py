3a8c7645356fbaa1168ec674c361d4d2d9f32969
agent-indigo <bradenhynes@gmail.com>
2025-05-30 23:55:57 -0230

    Expanded UUIDAutoField inheritance

--- btre/UUIDAutoField.py
+++ btre/UUIDAutoField.py
@@ -4,11 +4,11 @@ Can be set as the DEFAULT_AUTO_FIELD
 in settings.py to use v4 UUIDs as
 primary keys
 """
-from django.db.models import AutoField
+from django.db.models import AutoField, UUIDField
 from uuid import UUID, uuid4
 from django.db.backends.base.base import BaseDatabaseWrapper
 from django.db.models import Model
-class UUIDAutoField(AutoField):
+class UUIDAutoField(AutoField, UUIDField):
   def __init__(
     self: object,
     *args: tuple[
@@ -48,3 +48,29 @@ class UUIDAutoField(AutoField):
         value
       )
     return value
+  def get_prep_value(
+    self: object,
+    value: UUID | None
+  ) -> UUID | None:
+    if value is None:
+      return None
+    if isinstance(
+      value,
+      UUID
+    ):
+      return value
+    return UUID(value)
+  def from_db_value(
+    self: object,
+    value: UUID | None,
+    expression: object,
+    connection: BaseDatabaseWrapper
+  ) -> UUID | None:
+    if value is None:
+      return None
+    if isinstance(
+      value,
+      UUID
+    ):
+      return value
+    return UUID(value)
